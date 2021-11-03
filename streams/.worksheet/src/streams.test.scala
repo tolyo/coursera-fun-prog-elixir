90e73706f461fd9b124a433f58b7cca2b946796a
anatoly <anatolyostrovsky@gmail.com>
2021-11-03 16:17:30 +0200

    Streams initial

--- streams/.worksheet/src/streams.test.scala
+++
@@ -1,8 +0,0 @@
-package streams
-
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(74); 
-  println("Welcome to the Scala worksheet")}
-  
-
-  
-}
--- streams/target/scala-2.10/classes/streams/test.sc
+++
@@ -1,8 +0,0 @@
-package streams
-
-object test {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-  
-
-  
-}
